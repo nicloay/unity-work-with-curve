c9e50a616cc2f2a5231dc97853bebd95de986210
nicloay <nicloay@gmail.com>
2014-01-13 11:46:53 +0400

    created curve with couple of keyframes

--- Assets/Editor/CreateAnimation.cs
+++ Assets/Editor/CreateAnimation.cs
@@ -6,14 +6,20 @@ using UnityEditorInternal;
 public class CreateAnimation : Editor {
 	public static string GAME_OBJECT_NAME="gameObject";
 	public static string ANIMATION_CLIP_PATH = "Assets/animation.anim";
+	public static Keyframe[]  keyframes = new Keyframe[]{
+		new Keyframe(0.0f,  0,0,0),
+		new Keyframe(0.1f,  1,0,0),
+		new Keyframe(0.2f,  0,0,0),
+		new Keyframe(0.3f,  2,0,0),
+		new Keyframe(0.4f, -2,0,0),
+	};
+
 
 	[MenuItem("Assets/animation")]
 	public static void createAnimation(){
 		GameObject go = GameObject.Find(GAME_OBJECT_NAME);
 		if (go != null){
 			AnimatorController animCtrl = AnimatorController.GetEffectiveAnimatorController(go.GetComponent<Animator>());
-			Debug.Log(animCtrl);
-
 			AssetDatabase.DeleteAsset(ANIMATION_CLIP_PATH);
 			AssetDatabase.DeleteAsset(
 				AssetDatabase.GetAssetPath( animCtrl)
@@ -27,10 +33,14 @@ public class CreateAnimation : Editor {
 			animator = go.AddComponent<Animator>();
 
 		AnimationClip animationClip = new AnimationClip();
+		AnimationUtility.SetAnimationType(animationClip, ModelImporterAnimationType.Generic);
 
+
+		AnimationCurve curve = new AnimationCurve(keyframes);
+		animationClip.SetCurve("", typeof(Transform),"localPosition.x", curve);
 
 		AssetDatabase.CreateAsset(animationClip, ANIMATION_CLIP_PATH);
-
+		AssetDatabase.SaveAssets();
 		AddClipToAnimatorComponent(go, animator, animationClip);
 	}
 	
