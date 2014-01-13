1d8cd874047af556cdc23093bbefdd0407757be0
nicloay <nicloay@gmail.com>
2014-01-13 13:48:42 +0400

    squared line curve works, can use it in m_IsActive of GameObject

--- Assets/Scripts/Editor/CreateAnimation.cs
+++ Assets/Scripts/Editor/CreateAnimation.cs
@@ -2,16 +2,17 @@
 using System.Collections;
 using UnityEditor;
 using UnityEditorInternal;
+using System.Reflection;
 
 public class CreateAnimation : Editor {
 	public static string GAME_OBJECT_NAME="gameObject";
 	public static string ANIMATION_CLIP_PATH = "Assets/animation.anim";
 	public static Keyframe[]  keyframes = new Keyframe[]{
-		new Keyframe(0.0f,  0,0,0),
-		new Keyframe(0.1f,  1,0,0),
-		new Keyframe(0.2f,  0,0,0),
-		new Keyframe(0.3f,  2,0,0),
-		new Keyframe(0.4f, -2,0,0),
+		new Keyframe(0.0f,  0),
+		new Keyframe(0.1f,  1),
+		new Keyframe(0.2f,  0),
+		new Keyframe(0.3f,  1),
+		new Keyframe(0.4f,  0)
 	};
 
 
@@ -28,6 +29,8 @@ public class CreateAnimation : Editor {
 		}
 
 		go = new GameObject(GAME_OBJECT_NAME);
+		GameObject go2 = new GameObject(GAME_OBJECT_NAME);
+		go2.transform.parent = go.transform;
 		Animator animator = go.GetComponent<Animator>();
 		if (animator == null)
 			animator = go.AddComponent<Animator>();
@@ -37,7 +40,10 @@ public class CreateAnimation : Editor {
 
 
 		AnimationCurve curve = new AnimationCurve(keyframes);
-		animationClip.SetCurve("", typeof(Transform),"localPosition.x", curve);
+		curve.setTangentModeConstant();
+		animationClip.SetCurve(GAME_OBJECT_NAME, typeof(GameObject),"m_IsActive", curve);
+
+		EditorWindow.GetWindow( Assembly.GetAssembly(typeof(UnityEditor.Editor)).GetType("UnityEditor.InspectorWindow")).SendEvent(EditorGUIUtility.CommandEvent("CurveChanged"));
 
 		AssetDatabase.CreateAsset(animationClip, ANIMATION_CLIP_PATH);
 		AssetDatabase.SaveAssets();
